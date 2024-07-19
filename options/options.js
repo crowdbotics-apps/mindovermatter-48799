75912444f150a18a5dc3c772359fa001f7d0c94d
Crowdbotics <team@crowdbotics.com>
2024-07-19 02:28:43 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mindovermatter-48799",
+  url: "https://mindovermatter-48799.botics.co",
+  api: "https://mindovermatter-48799.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
